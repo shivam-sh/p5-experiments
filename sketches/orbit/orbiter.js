96e3f0becaac2bf136c6b4e2bc4dffac68fb0ae8
Shivam Sharma <shivam.sharm@icloud.com>
2020-05-08 21:42:41 -0400

    Updated physics and graphics

--- sketches/orbit/orbiter.js
+++ sketches/orbit/orbiter.js
@@ -3,45 +3,35 @@ let showVectors = false
 class Orbiter {
     constructor(x, y, z, m) {
         this.pos = createVector(x, y, z)
-        this.vel = createVector( random(-5, 5), random(-5, 5), random(-5, 5))
+        this.vel = p5.Vector.random3D()
+        this.vel.mult(5)
         this.acc = createVector(0, 0, 0)
         this.mass = m
-        this.rad = sqrt(m) * 5
+        this.rad = sqrt(m)
         this.size = 0;
     }
 
     applyForce(f) {
-        this.acc.set(f)
+        this.acc.set(f.div(this.mass))
     }
 
     update() {
         this.vel.add(this.acc)
         this.pos.add(this.vel)
-
-        // loop the orbiter around the screen if it exits the boundaries
-        if (this.pos.x < 0) {
-        //    this.pos.x += width
-        } else if (this.pos.x > width) {
-        //    this.pos.x -= width
-        }
-        if (this.pos.y < 0) {
-        //    this.pos.y += height
-        } else if (this.pos.y > height) {
-        //    this.pos.y -= height
-        }
     }
 
     show() {
-        fill(220)
+        var distanceBasedRadius = this.rad * map(this.pos.z, -500, 500, 1, 4)
+        fill(255)
         strokeWeight(2)
-        stroke(220)
-        ellipse(this.pos.x, this.pos.y, this.rad * map(this.pos.z, -100, 100, 1.6, 2.4))
+        stroke(255)
+        ellipse(this.pos.x, this.pos.y, distanceBasedRadius)
 
         if (showVectors) {
             strokeWeight(4)
+            
             stroke(50, 50, 200)
             line(this.pos.x, this.pos.y, this.pos.x + this.vel.x, this.pos.y + this.vel.y)
-            stroke
             stroke(200, 50, 50)
             line(this.pos.x, this.pos.y, this.pos.x + this.acc.x, this.pos.y + this.acc.y)
             stroke(200)
--- sketches/orbit/sketch.js
+++ sketches/orbit/sketch.js
@@ -1,32 +1,35 @@
 // A simple simulation to practice working with vectors in p5.js
 // This code is a customized version of the gravitational orbit ecanple created by Dan Schiffman
 
-var numOrbiting = 5;
+var numOrbiting = 10;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  attractor = new Attractor(width/2, height/2, 50);
+  background(0, 0, 0);
+
+  attractor = new Attractor(width/2, height/2, 900);
   orbiters = [numOrbiting];
 
   for (let i = 0; i < numOrbiting; i++) {
-    orbiters[i] = new Orbiter(random(0, width), random(0, height), random(10, -10), random(5, 20));
+    orbiters[i] = new Orbiter(random(0, width), random(0, height), random(-30, -30), random(80, 200));
   }
 }
 
 function draw() {
-  background(190, 190, 190, 200);
+  background(0, 0, 0, 30);
 
   for (let i = 0; i < numOrbiting; i++) {
     attractor.attract(orbiters[i]);
     orbiters[i].update();
-  
+  }
+
+  for (let i = 0; i < numOrbiting; i++) {
     if (orbiters[i].pos.z >= 0) {
-      attractor.show();
       orbiters[i].show();
+      attractor.show();
     } else {
-      orbiters[i].show();
       attractor.show();
+      orbiters[i].show();
     }
   }
-  console.log(orbiters[1].pos.z)
 }
