10a04137233254844c09d606cc2972967dc65c7f
Shivam Sh <shivam.sharm@icloud.com>
2020-07-24 17:58:52 -0400

    a* - Implemented main pathfinding loop

--- sketches/a*/node.js
+++ sketches/a*/node.js
@@ -1,26 +1,28 @@
 const states = {
-    OPEN: 'open',
-    BLOCKED: 'blocked',
-    START: 'start-node',
-    FINISH: 'end-node',
-    QUEUED: 'queued',
-    CLOSED: 'closed'
-}
+	OPEN: "open",
+	BLOCKED: "blocked",
+	START: "start-node",
+	FINISH: "end-node",
+	QUEUED: "queued",
+	CLOSED: "closed",
+	PATH: "path",
+	FAILED: "failed"
+};
 
 class MapNode {
 	constructor(x, y, w, h, state) {
-        // f, g, and h values
-        this.f = 0;
-        this.g = 0;
-        this.h = 0;
+		// f, g, and h values
+		this.f = Infinity;
+		this.g = Infinity;
+		this.h = Infinity;
 
-        // Array to store all connections
-        this.connected = [];
+		// Array to store all connections
+		this.connected = [];
 
-        // Ref to previous node
-        this.cameFrom = undefined;
+		// Ref to previous node
+		this.cameFrom = undefined;
 
-        // Data for visuals
+		// Data for visuals
 		this.xPos = x;
 		this.yPos = y;
 		this.width = w;
--- sketches/a*/sketch.js
+++ sketches/a*/sketch.js
@@ -1,3 +1,6 @@
+// Init
+let percentFilled = 60;
+
 // Values for graphical output
 let nodeHeight = 30;
 let nodeWidth = 30;
@@ -12,8 +15,8 @@ function setup() {
 	background(0, 0, 0);
 
 	// Setup values for grid
-	let columns = Math.floor(width / nodeWidth);
-	let rows = Math.floor(height / nodeHeight);
+	let columns = floor(width / nodeWidth);
+	let rows = floor(height / nodeHeight);
 
 	// Create 2D grid
 	let grid = new Array(columns);
@@ -24,7 +27,100 @@ function setup() {
 	// Create storage for nodes
 	let queue = [];
 	let closed = [];
+
+	// Fill the grid with nodes based on the % that should be open
+	for (let x = 0; x < columns; x++) {
+		for (let y = 0; y < rows; y++) {
+			if (Math.random() * 100 < percentFilled) {
+				grid[x][y] = new MapNode(
+					x * nodeWidth,
+					y * nodeHeight,
+					nodeWidth,
+					nodeHeight,
+					states.OPEN
+				);
+			} else {
+				grid[x][y] = new MapNode(
+					x * nodeWidth,
+					y * nodeHeight,
+					nodeWidth,
+					nodeHeight,
+					states.CLOSED
+				);
+			}
+		}
+	}
+
+	// Set start and end points
+	let start = grid[0][0];
+	let finish = grid[columns - 1][rows - 1];
+	start.setState(states.START);
+	start.g = 0;
+	finish.setState(states.FINISH);
+
+	// Add the start node to the queue
+	queue.push(start);
 }
 
 function draw() {
+	if (queue.length > 0) {
+		// Run main search
+		let currentNode = queue[0];
+		let newNodes = currentNode.getConnections();
+
+		// Draw the path if done
+		if (currentNode == finish) {
+			queue = [];
+			while (currentNode.cameFrom() != undefined) {
+				currentNode.setState(states.PATH);
+				currentNode = currentNode.cameFrom;
+			}
+		}
+		
+		// Remove node from queue
+		closed.push(queue[0]);
+		queue.shift();
+		
+		// Insert each new node into the queue according to its f score
+		newNodes.forEach((node) => {
+			// Check if this is the quickest path to this node
+			let tempG =
+			currentNode.g +
+			dist(currentNode.x, currentNode.y, node.x, node.y);
+			if (tempG < node.g) {
+				node.cameFrom = currentNode;
+				node.g = tempG;
+				node.f = node.g + dist(node.x, node.y, finish.x, finish.y);
+			}
+			
+			// Sort through the array to find where to position the new node
+			let lowerBound = 0;
+			let upperBound = queue.length - 1;
+			while (lowerBound < upperBound) {
+				let pivot = floor((lowerBound + upperBound) / 2);
+				
+				if (f <= queue[pivot].getF()) {
+					upperBound = pivot;
+				} else if (f > queue[pivot].getF()) {
+					lowerBound = pivot;
+				}
+			}
+			
+			queue.splice(lowerBound, 0, node);
+		});
+	} else {
+		// Search failed, end task
+		currentNode = closed[closed.length - 1];
+		while (currentNode.cameFrom() != undefined) {
+			currentNode.setState(states.PATH);
+			currentNode = currentNode.cameFrom;
+		}
+	}
+	
+	// Output the grid
+	for (let x = 0; x < columns; x++) {
+		for (let y = 0; y < rows; y++) {
+			grid[x][y].show();
+		}
+	}
 }
