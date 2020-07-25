50094f368e283345af86ab85cd959ab439d721ac
Shivam Sh <shivam.sharm@icloud.com>
2020-07-25 14:02:49 -0400

    A* Tutorial - Data Structures

--- sketches/a*/node.js
+++ sketches/a*/node.js
@@ -1,24 +1,24 @@
 const states = {
 	OPEN: "open",
 	BLOCKED: "blocked",
-	FINISH: "end-node",
+	GOAL: "goal-node",
 	QUEUED: "queued",
 	CLOSED: "closed",
-	FOUND: "found",
+	SUCCESS: "success",
 	FAILED: "failed",
 };
 
 class MapNode {
 	constructor(c, r, w, h, state) {
 		// f, g, and h values
-		this.f = -1;
-		this.g = -1;
-		this.h = -1;
+		this.f = Infinity;
+		this.g = Infinity;
+		this.h = Infinity;
 
 		// Array to store all connections
 		this.connected = [];
 
-		// Ref to previous node
+		// Reference to previous node
 		this.cameFrom = undefined;
 
 		// Data for visuals
@@ -27,94 +27,5 @@ class MapNode {
 		this.width = w;
 		this.height = h;
 		this.state = state;
-	}
-
-	getConnections(grid) {
-		if (!this.connected == []) {
-			if (
-				this.column != 0 &&
-				(grid[this.column - 1][this.row].state == states.OPEN ||
-				grid[this.column - 1][this.row].state == states.FINISH)
-			) {
-				this.connected.push(grid[this.column - 1][this.row]);
-			}
-			if (
-				this.row != 0 &&
-				(grid[this.column][this.row - 1].state == states.OPEN ||
-				grid[this.column][this.row - 1].state == states.FINISH)
-			) {
-				this.connected.push(grid[this.column][this.row - 1]);
-			}
-			if (
-				this.column != grid.length - 1 &&
-				(grid[this.column + 1][this.row].state == states.OPEN ||
-				grid[this.column + 1][this.row].state == states.FINISH)
-			) {
-				this.connected.push(grid[this.column + 1][this.row]);
-			}
-			if (
-				this.row != grid[0].length - 1 &&
-				(grid[this.column][this.row + 1].state == states.OPEN ||
-				grid[this.column][this.row + 1].state == states.FINISH)
-			) {
-				this.connected.push(grid[this.column][this.row + 1]);
-			}
-
-			return this.connected;
-		} else {
-			return this.connected;
-		}
-	}
-
-	show() {
-		switch (this.state) {
-			case states.OPEN:
-				fill(255, 255, 255);
-				break;
-			case states.BLOCKED:
-				fill(50, 50, 50);
-				break;
-			case states.FINISH:
-				fill(100, 100, 200);
-				break;
-			case states.QUEUED:
-				fill(255, 200, 100);
-				break;
-			case states.CLOSED:
-				fill(150, 150, 150);
-				break;
-			case states.FOUND:
-				fill(50, 200, 50);
-				break;
-			case states.FAILED:
-				fill(200, 100, 100);
-				break;
-		}
-
-
-		let canvasDiv = document.getElementById("canvas");
-		let width = canvasDiv.offsetWidth;
-		let height = canvasDiv.offsetHeight;
-
-		let xOffset = (width % this.width) / 2;
-		let yOffset = (height % this.height) / 2;
-
-		strokeWeight(2);
-		stroke(0);
-		rect(
-			this.column * this.width + xOffset,
-			this.row * this.height + yOffset,
-			this.width,
-			this.height,
-			4
-		);
-	}
-
-	setState(state) {
-		this.state = state;
-	}
-
-	setCameFrom(node) {
-		this.cameFrom = node;
 	}
 }
--- sketches/a*/sketch.js
+++ sketches/a*/sketch.js
@@ -1,10 +1,3 @@
-// Init
-let percentFilled = 20;
-
-// Values for graphical output
-let nodeHeight = 30;
-let nodeWidth = 30;
-
 // GLOBAL VARIABLES
 let queue = [];
 let closed = [];
@@ -12,8 +5,6 @@ let closed = [];
 let grid;
 let columns;
 let rows;
-let start;
-let finish;
 
 function setup() {
 	// Code to create an adaptive canvas
@@ -34,146 +25,9 @@ function setup() {
 		grid[i] = new Array(rows);
 	}
 
-	// Fill the grid with nodes based on the % that should be open
-	for (let x = 0; x < columns; x++) {
-		for (let y = 0; y < rows; y++) {
-			if (Math.random() * 100 > percentFilled) {
-				grid[x][y] = new MapNode(
-					x,
-					y,
-					nodeWidth,
-					nodeHeight,
-					states.OPEN
-				);
-			} else {
-				grid[x][y] = new MapNode(
-					x,
-					y,
-					nodeWidth,
-					nodeHeight,
-					states.BLOCKED
-				);
-			}
-		}
-	}
-
-	// Set start and end points
-	start = grid[0][0];
-	finish = grid[columns - 1][rows - 1];
-	start.setState(states.OPEN);
-	start.g = 0;
-	finish.setState(states.FINISH);
-
-	// Add the start node to the queue
-	queue.push(start);
-
-	// Output the base
-	for (let x = 0; x < columns; x++) {
-		for (let y = 0; y < rows; y++) {
-			grid[x][y].show();
-		}
-	}
+	// Create storage for nodes
+	queue = [];
+	closed = [];
 }
 
-function draw() {
-	// Output the nodes
-	for (let x = 0; x < columns; x++) {
-		for (let y = 0; y < rows; y++) {
-			if (grid[x][y].state == states.QUEUED) {
-				grid[x][y].show();
-			}
-		}
-	}
-
-
-	if (queue.length > 0) {
-		// Run main search
-		let currentNode = queue[0];
-		let newNodes = currentNode.getConnections(grid);
-
-		newNodes.forEach((node) => {
-			node.setState(states.QUEUED);
-		});
-
-		// Draw the path if done
-		if (currentNode == finish) {
-			queue = [];
-			newNodes = [];
-			while (currentNode.cameFrom != undefined) {
-				currentNode.setState(states.FOUND);
-				currentNode.show();
-				currentNode = currentNode.cameFrom;
-			}
-			start.setState(states.FOUND);
-			start.show();
-		} else {
-			// Remove node from queue
-			currentNode.setState(states.CLOSED);
-			currentNode.show();
-			closed.push(currentNode);
-			queue.shift();
-
-			// Insert each new node into the queue according to its f score
-			newNodes.forEach((node) => {
-				// Check if this is the quickest path to this node
-				let tempG;
-				if (node.g == -1) {
-					tempG = dist(
-						currentNode.column * nodeWidth,
-						currentNode.row * nodeHeight,
-						node.column * nodeWidth,
-						node.row * nodeHeight
-					);
-				} else {
-					tempG =
-						currentNode.g +
-						dist(
-							currentNode.column * nodeWidth,
-							currentNode.row * nodeHeight,
-							node.column * nodeWidth,
-							node.row * nodeHeight
-						);
-				}
-
-				if (tempG < node.g || node.g == -1) {
-					node.cameFrom = currentNode;
-					node.g = tempG;
-					node.f =
-						node.g +
-						dist(
-							node.column * nodeWidth,
-							node.row * nodeHeight,
-							finish.column * nodeWidth,
-							finish.row * nodeHeight
-						);
-				}
-
-				// Sort through the array to find where to position the new node
-				let lowerBound = 0;
-				let upperBound = queue.length - 1;
-				while (lowerBound < upperBound) {
-					let pivot = floor((lowerBound + upperBound) / 2);
-
-					if (node.f <= queue[pivot].f) {
-						upperBound = pivot - 1;
-					} else if (node.f > queue[pivot].f) {
-						lowerBound = pivot + 1;
-					}
-				}
-				node.setState(states.QUEUED);
-				node.show()
-				queue.splice(lowerBound, 0, node);
-			});
-		}
-	} else if (start.state != states.FOUND) {
-		// Search failed, end task
-		for (let x = 0; x < columns; x++) {
-			for (let y = 0; y < rows; y++) {
-				if (grid[x][y].state == states.CLOSED) {
-					grid[x][y].setState(states.FAILED);
-					grid[x][y].show();
-				}
-			}
-		}
-	}
-}
+function draw() {}
